24a3d38623c0c17b7724955c7c72c1b7c012e072
rayhan <rayhan@gmail.com>
2023-05-02 00:12:19 +0600

    React Learning

---
+++ 01/first_app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Pages/Home';
+import About from './Pages/About';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
