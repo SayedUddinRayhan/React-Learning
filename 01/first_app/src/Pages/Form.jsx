715093b568ea577f9dff3dd1fcfd2ac5a60cad61
SayedUddinRayhan <sayedrayhan10@gmail.com>
2023-05-10 23:51:09 +0600

    Learned select box form

--- 01/first_app/src/Pages/Form.jsx
+++ 01/first_app/src/Pages/Form.jsx
@@ -9,6 +9,13 @@ export default class Form extends Component {
             <div className='container'>
                 <input type="text" name='text' placeholder='Enter your name' className='form-control' onChange={this.change} />
 
+                <select name="category" className='form-control' onChange={this.change}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                </select>
+
             </div>
         )
     }
